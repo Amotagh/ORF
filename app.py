5c410b649c77e824652d6787ac4f3ee2805a40df
atieh motaghi <atieh.motaghi@gmail.com>
2022-10-28 14:42:22 -0400

    flask made

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask
-import numpy as np 
+from operator import methodcaller
+import re
+from flask import Flask, render_template,request
+import pandas as pd
+import numpy as np
+import sklearn
 from joblib import load
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def hello_world():
-    test=['Establishment of the Intelligent Visualization Laboratory']
-    model_tfidf=load('model_tfidf.joblib')
-    data=model_tfidf.tranform(test)
-    return data
+    request_type_str=request.method
+    if request_type_str == 'GET':
+        return render_template('index.html')
+    
+        text=request.form['text']
+        model_tfidf=load('model_tfidf.joblib')
+        data=model_tfidf.transform(text)
+        model_logistic=load('model.joblib')
+        pred = model_logistic.predict (data)
+        if pred[0] == 0:
+             return render_template('index0.html')  
+        else:
+             return render_template('index1.html')
+
+
+        
+
+    
+
+    
